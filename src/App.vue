fc94a94993c6ca6f30594e22ebb317245ae0ec84
Thomas Kientz <thomas@kientz.fr>
2019-12-29 20:43:59 +0100

    Reproduce issue

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    foo : {{ foo.bar }}
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+
+const obj = { bar: "baz" };
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     HelloWorld
-  }
-}
+  },
+  data: () => ({
+    foo: { ...obj }
+  })
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
